adea89af88aaf2747d54f72621ea12fee8b927c9
Juan Lucero <luceroweb@gmail.com>
2023-08-24 14:49:00 -0700

    chore(destination card): create component

---
+++ src/components/DestinationCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function DestinationCard({ destination }) {
+  return (
+    <div className="mt-6 grid gap-6 lg:grid-cols2 xl:grid-cols-3">
+      <div className="flex items-center rounded-lg bg-white shadow-lg overflow-hidden">
+        <img
+          className="h-32 w-32 flex-shrink-0"
+          src={destination.imageUrl}
+          alt={destination.imageAlt}
+        />
+        <div className="px-6 py-4">
+          <h3 className="text-lg font-semibold text-gray-800 uppercase">
+            {destination.city}
+          </h3>
+          <p className="text-gray-600">
+            ${destination.averagePrice} / night average
+          </p>
+          <div className="mt-4">
+            <a
+              href="#"
+              className="text-indigo-500 hover:text-indigo-400 font-semibold text-sm"
+            >
+              Explore {destination.propertyCount} properties
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
